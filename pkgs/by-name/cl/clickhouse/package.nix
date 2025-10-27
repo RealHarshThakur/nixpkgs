76006674a37eb4b016f4947bc7178793eb515e19
Harsh <harsh@nuon.co>
2025-10-28 00:17:34 +0530

    fix: clickhouse hash mismatch error
    Signed-off-by: harsh <harsh@nuon.co>

--- pkgs/by-name/cl/clickhouse/package.nix
+++ pkgs/by-name/cl/clickhouse/package.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
   version = "25.9.4.58-stable";
-  hash = "sha256-HRbqVSyDuvhkv0+PSgps9AXKdLlukrLA65OLx5gZ3c0=";
+  hash = "sha256-p3nG09z+wO8+2L1H2FGnsV+7gPnL3le93QLwK4uC8fg=";
   lts = false;
   nixUpdateExtraArgs = [
     "--version-regex"
